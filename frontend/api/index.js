e69bfe822c77009114c27cafd15429c4707b0a89
Carolina <csa71@gcloud.ua.es>
2026-09-09 15:35:48 +0200

    deploy

--- frontend/api/index.js
+++ frontend/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const Groq = require('groq-sdk');
+const Groq = require('groq-sdk/index.js');
 
 const app = express();
 app.use(cors());
